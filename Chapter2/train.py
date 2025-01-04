ed8b2ee445f6bf2d9434bd289a3c692d36fd4df6
als8921 <als8921@naver.com>
2025-01-04 21:47:00 +0900

    2. Chapter 학습 내용 추가

--- Chapter2/train.py
+++ Chapter2/train.py
@@ -9,7 +9,20 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 from matplotlib.colors import ListedColormap
-
+""" Chapter 2
+- 인공 뉴런: 초기 머신 러닝의 간단한 역사
+    - 인공 뉴런의 수학적 정의
+    - 퍼셉트론 학습 규칙
+- 파이썬으로 퍼셉트론 학습 알고리즘 구현
+    - 객체 지향 퍼셉트론 API
+    - 붓꽃 데이터셋에서 퍼셉트론 훈련
+- 적응형 선형 뉴런과 학습의 수렴
+    - 경사 하강법으로 비용 함수 최소화
+    - 파이썬으로 아달린 구현
+    - 특성 스케일을 조정하여 경사 하강법 결과 향상
+    - 대규모 머신 러닝과 확률적 경사 하강법
+- 요약
+"""
 ### DATA ###
 df = iris_data.df
 
