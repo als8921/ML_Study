69b4ad8456b1419a23684cc6834a5843808c2ff2
als8921 <als8921@naver.com>
2025-01-07 22:57:22 +0900

    3.4 SVM Support Vector Machine

--- Chapter3/train.py
+++ Chapter3/train.py
@@ -66,6 +66,9 @@ sc.fit(X_train)
 X_train_std = sc.transform(X_train)
 X_test_std = sc.transform(X_test)
 
+X_combined_std = np.vstack((X_train_std, X_test_std))
+y_combined = np.hstack((y_train, y_test))
+
 X_train_01_subset = X_train_std[(y_train == 0) | (y_train == 1)]
 y_train_01_subset = y_train[(y_train == 0) | (y_train == 1)]
 
@@ -106,4 +109,20 @@ def SKLearnLogisticRegression():
     plt.tight_layout()
     plt.show()
 
-SKLearnLogisticRegression()
+def SKLearnSupportVectorMachine():
+    from sklearn.svm import SVC
+
+    svm = SVC(kernel='linear', C=1.0, random_state=1)
+    svm.fit(X_train_std, y_train)
+
+    plot_decision_regions(X_combined_std,
+                        y_combined,
+                        classifier=svm,
+                        test_idx=range(105, 150))
+    plt.xlabel('Petal length [standardized]')
+    plt.ylabel('Petal width [standardized]')
+    plt.legend(loc='upper left')
+    plt.tight_layout()
+    plt.show()
+
+SKLearnSupportVectorMachine()
