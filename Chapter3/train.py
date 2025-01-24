6d28fc4ba0feddc68d074c5d7c31172508cc3926
als8921 <als8921@naver.com>
2025-01-24 21:34:46 +0900

    3.6.2 결정 트리 만들기 붓꽃 데이터에 적용

--- Chapter3/train.py
+++ Chapter3/train.py
@@ -141,4 +141,26 @@ def SKLearnKernelSVM():
     #plt.savefig('figures/03_15.png', dpi=300)
     plt.show()
 
-SKLearnKernelSVM()
+def DecisionTree():
+
+    from sklearn.tree import DecisionTreeClassifier
+
+    tree_model = DecisionTreeClassifier(criterion='gini',
+                                        max_depth=4,
+                                        random_state=1)
+    tree_model.fit(X_train, y_train)
+
+    X_combined = np.vstack((X_train, X_test))
+    y_combined = np.hstack((y_train, y_test))
+    plot_decision_regions(X_combined, y_combined,
+                        classifier=tree_model,
+                        test_idx=range(105, 150))
+
+    plt.xlabel('Petal length [cm]')
+    plt.ylabel('Petal width [cm]')
+    plt.legend(loc='upper left')
+    plt.tight_layout()
+    #plt.savefig('figures/03_20.png', dpi=300)
+    plt.show()
+    
+DecisionTree()
