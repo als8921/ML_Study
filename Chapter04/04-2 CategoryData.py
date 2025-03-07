2bb94e0f43e14c141f723e9e6e0d1016d60dd3d1
als8921 <als8921@naver.com>
2025-03-07 22:50:42 +0900

    4.2.4 순서가 없는 특성 레이블링, 데이터 변환

--- Chapter04/04-2 CategoryData.py
+++ Chapter04/04-2 CategoryData.py
@@ -7,7 +7,7 @@ df = pd.DataFrame([['green', 'M', 10.1, 'class2'],
                 ['blue', 'XL', 15.3, 'class2']])
 
 df.columns = ['color', 'size', 'price', 'classlabel']
-
+print(df.values)
 
 def process_4_2_1():
     pass
@@ -52,4 +52,65 @@ def process_4_2_3():
     print(class_le.inverse_transform(y))
 
 
-process_4_2_3()
+def process_4_2_4():
+    from sklearn.preprocessing import LabelEncoder
+    
+    X = df[['color', 'size', 'price']].values
+    color_le = LabelEncoder()
+    X[:, 0] = color_le.fit_transform(X[:, 0])   # 0열을 LabelEncoder를 사용하여 레이블링
+    # print(X)
+
+    #####################
+    from sklearn.compose import ColumnTransformer
+    from sklearn.preprocessing import OrdinalEncoder
+
+    ord_enc = OrdinalEncoder(dtype=int)
+    col_trans = ColumnTransformer([('ord_enc', ord_enc, ['color'])])
+    X_trans = col_trans.fit_transform(df)
+    # print(X_trans)
+    # print(col_trans.named_transformers_['ord_enc'].inverse_transform(X_trans))
+
+    ###############
+    # OneHotEncoder 를 사용한 특성의 고유값을 만들어 변환
+    from sklearn.preprocessing import OneHotEncoder
+
+    X = df[['color', 'size', 'price']].values
+    color_ohe = OneHotEncoder()
+    # print(color_ohe.fit_transform(X[:, 0].reshape(-1, 1)).toarray())
+
+    ####################################################################################################
+    # 
+    # 1. ColumnTransformer 를 사용하여 blue, green, red [0, 0, 0] 행을 만들어 특성으로 넣기
+    from sklearn.compose import ColumnTransformer
+
+    # 4_2_2 map 함수를 사용하여 사이즈 데이터를 정수로 변환
+    size_mapping = {'XL': 3, 'L': 2, 'M': 1}
+    df['size'] = df['size'].map(size_mapping)
+
+    X = df[['color', 'size', 'price']].values
+    c_transf = ColumnTransformer([ ('onehot', OneHotEncoder(), [0]),
+                                ('nothing', 'passthrough', [1, 2])])
+    print(c_transf.fit_transform(X).astype(float))
+    # OneHotEncoder에서 다중 공선성 문제 처리 (blue 열 삭제)
+    c_transf = ColumnTransformer([ ('onehot', OneHotEncoder(categories='auto', drop='first'), [0]),
+                                ('nothing', 'passthrough', [1, 2])])
+    print(c_transf.fit_transform(X).astype(float))
+
+    # 2. pd.get_dummies() 함수를 사용하여 위의 기능 구현
+    print(pd.get_dummies(df[['price', 'color', 'size']], columns=['color']))
+    print(pd.get_dummies(df[['price', 'color', 'size']], columns=['color'], drop_first=True))
+
+    ###################################################################################################
+    # 순서가 있는 특성 인코딩
+    ddf = pd.DataFrame([['green', 'M', 10.1, 'class2'],
+                   ['red', 'L', 13.5, 'class1'],
+                   ['blue', 'XL', 15.3, 'class2']])
+
+    ddf.columns = ['color', 'size', 'price', 'classlabel']
+    print(ddf)
+    ddf['x > M'] = ddf['size'].apply(lambda x: 1 if x in {'L', 'XL'} else 0)
+    ddf['x > L'] = ddf['size'].apply(lambda x: 1 if x == 'XL' else 0)
+
+    del ddf['size']
+    print(ddf)
+process_4_2_4()
