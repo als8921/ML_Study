b89e7ae1fc7f85ea23d393a1e15ee3372ebb7ff1
als8921 <als8921@naver.com>
2025-03-09 22:45:54 +0900

    4.5.3 Regularization Parameter 변경에 따른 W 가중치의 변화

--- Chapter04/04-5 유용한 특성 선택.py
+++ Chapter04/04-5 유용한 특성 선택.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
@@ -41,3 +42,39 @@ print('훈련 정확도:', lr.score(X_train_std, y_train))
 print('테스트 정확도:', lr.score(X_test_std, y_test))
 print(lr.intercept_)
 print(lr.coef_)
+
+# L1 Regularization
+##########################################################################################
+fig = plt.figure()
+ax = plt.subplot(111)
+
+colors = ['blue', 'green', 'red', 'cyan',
+          'magenta', 'yellow', 'black',
+          'pink', 'lightgreen', 'lightblue',
+          'gray', 'indigo', 'orange']
+
+weights, params = [], []
+for c in np.arange(-4., 6.):
+    lr = LogisticRegression(penalty='l1', C=10.**c, solver='liblinear',
+                            multi_class='ovr', random_state=0)
+    lr.fit(X_train_std, y_train)
+    weights.append(lr.coef_[1])
+    params.append(10**c)
+
+weights = np.array(weights)
+
+for column, color in zip(range(weights.shape[1]), colors):
+    plt.plot(params, weights[:, column],
+             label=df_wine.columns[column + 1],
+             color=color)
+plt.axhline(0, color='black', linestyle='--', linewidth=3)
+plt.xlim([10**(-5), 10**5])
+plt.ylabel('Weight coefficient')
+plt.xlabel('C (inverse regularization strength)')
+plt.xscale('log')
+plt.legend(loc='upper left')
+ax.legend(loc='upper center',
+          bbox_to_anchor=(1.38, 1.03),
+          ncol=1, fancybox=True)
+
+plt.show()
