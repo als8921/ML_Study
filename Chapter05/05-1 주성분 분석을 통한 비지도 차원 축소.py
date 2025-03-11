bbc042ff351d20bb45df640e337c695a276b125f
als8921 <als8921@naver.com>
2025-03-11 19:56:12 +0900

    5.1.5 사이킷런에서의 PCA

--- Chapter05/05-1 주성분 분석을 통한 비지도 차원 축소.py
+++ Chapter05/05-1 주성분 분석을 통한 비지도 차원 축소.py
@@ -74,21 +74,82 @@ eigen_pairs.sort(key=lambda k: k[0], reverse=True)
 # 2개의 고유벡터로 투영 행렬 만들기
 w = np.hstack((eigen_pairs[0][1][:, np.newaxis],
                eigen_pairs[1][1][:, np.newaxis]))
-print('투영 행렬 W:\n', w)
-
-print(X_train_std[0].dot(w))
+# print('투영 행렬 W:\n', w)
+# print(X_train_std[0].dot(w))
 
 X_train_pca = X_train_std.dot(w)
-print("X_train_pca")
-print(len(X_train_pca))
+# print("X_train_pca")
+# print(len(X_train_pca))
 colors = ['r', 'b', 'g']
 markers = ['o', 's', '^']
 
-for l, c, m in zip(np.unique(y_train), colors, markers):
-    plt.scatter(X_train_pca[y_train == l, 0],
-                X_train_pca[y_train == l, 1],
-                c=c, label=f'Class {l}', marker=m)
+# for l, c, m in zip(np.unique(y_train), colors, markers):
+#     plt.scatter(X_train_pca[y_train == l, 0],
+#                 X_train_pca[y_train == l, 1],
+#                 c=c, label=f'Class {l}', marker=m)
 
+# plt.xlabel('PC 1')
+# plt.ylabel('PC 2')
+# plt.legend(loc='lower left')
+# plt.tight_layout()
+# plt.show()
+
+
+#######################################################################################
+# 사이킷런의 주성분 분석
+
+from matplotlib.colors import ListedColormap
+from sklearn.decomposition import PCA
+from sklearn.linear_model import LogisticRegression
+
+pca = PCA()
+X_train_pca = pca.fit_transform(X_train_std)
+
+
+def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
+
+    # 마커와 컬러맵을 준비합니다
+    markers = ('o', 's', '^', 'v', '<')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # 결정 경계를 그립니다
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    lab = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    lab = lab.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, lab, alpha=0.3, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    # 클래스별로 샘플을 그립니다
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0],
+                    y=X[y == cl, 1],
+                    alpha=0.8,
+                    c=colors[idx],
+                    marker=markers[idx],
+                    label=f'Class {cl}',
+                    edgecolor='black')
+        
+
+pca = PCA(n_components=2)
+X_train_pca = pca.fit_transform(X_train_std)
+X_test_pca = pca.transform(X_test_std)
+
+lr = LogisticRegression(random_state=1, solver='lbfgs')
+lr = lr.fit(X_train_pca, y_train)
+
+plot_decision_regions(X_train_pca, y_train, classifier=lr)
+plt.xlabel('PC 1')
+plt.ylabel('PC 2')
+plt.legend(loc='lower left')
+plt.tight_layout()
+plt.show()
+
+plot_decision_regions(X_test_pca, y_test, classifier=lr)
 plt.xlabel('PC 1')
 plt.ylabel('PC 2')
 plt.legend(loc='lower left')
